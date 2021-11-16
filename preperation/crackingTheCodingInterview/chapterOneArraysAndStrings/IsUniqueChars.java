8e273d8befda6c682b0c5f16f7a486b36c7c8957
Y.Yunus AKKOYUNLU <y.yunus.akkoyunlu@gmail.com>
2021-11-16 18:09:52 +0300

    cracking the coding interview chapter one arrays and string Is Unique solution

---
+++ preperation/crackingTheCodingInterview/chapterOneArraysAndStrings/IsUniqueChars.java
@@ -0,0 +1,26 @@
+package preperation.crackingTheCodingInterview.chapterOneArraysAndStrings;
+
+/*
+    Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?
+* */
+
+public class IsUniqueChars {
+
+    boolean isUniqueChars(String str) {
+        if (str.length() > 128) {
+            return false;
+        }
+
+        boolean[] charSet = new boolean[128];
+        for (int i = 0; i < str.length(); i++) {
+            int value = str.charAt(i);
+            if (charSet[value]) { // if that char added before
+                return false;
+            }
+            charSet[value] = true;
+        }
+
+        return true;
+    }
+
+}
