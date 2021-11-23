488b4aead5d7a1904c4fb0ac0b6220c6d563b557
Y.Yunus AKKOYUNLU <y.yunus.akkoyunlu@gmail.com>
2021-11-23 23:44:23 +0300

    Palindrome Permutation resolved

---
+++ preperation/crackingTheCodingInterview/chapterOneArraysAndStrings/IsPermutationOfPalindrome.java
@@ -0,0 +1,50 @@
+package preperation.crackingTheCodingInterview.chapterOneArraysAndStrings;
+
+public class IsPermutationOfPalindrome {
+
+    public static boolean isPermutationOfPalindrome(String phrase) {
+        int[] table = buildCharFrequencyTable(phrase);
+
+        return checkMaxOneOdd(table);
+    }
+
+    public static boolean checkMaxOneOdd(int[] table) {
+        boolean foundOdd = false;
+        for (int count : table) {
+            if (count % 2 == 1) {
+                if (foundOdd) {
+                    return false;
+                }
+                foundOdd = true;
+            }
+        }
+        return true;
+    }
+
+    public static int[] buildCharFrequencyTable(String phrase) {
+        int[] table = new int[Character.getNumericValue('z')
+                - Character.getNumericValue('a') + 1];
+
+        for (char c : phrase.toCharArray()) {
+            int x = getCharNumber(c);
+            if (x != -1) {
+                table[x]++;
+            }
+        }
+
+        return table;
+    }
+
+    public static int getCharNumber(char c) {
+        int a = Character.getNumericValue('a');
+        int z = Character.getNumericValue('z');
+        int value = Character.getNumericValue(c);
+
+        if (a <= value && value <= z) {
+            return z - value;
+        }
+
+        return -1;
+    }
+
+}
