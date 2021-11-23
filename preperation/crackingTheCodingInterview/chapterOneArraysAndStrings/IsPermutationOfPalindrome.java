e33d00e7f326f630c795031c4f878ddd731372de
Y.Yunus AKKOYUNLU <y.yunus.akkoyunlu@gmail.com>
2021-11-24 00:18:14 +0300

    Palindrome Permutation resolved V2

--- preperation/crackingTheCodingInterview/chapterOneArraysAndStrings/IsPermutationOfPalindrome.java
+++ preperation/crackingTheCodingInterview/chapterOneArraysAndStrings/IsPermutationOfPalindrome.java
@@ -8,6 +8,26 @@ public class IsPermutationOfPalindrome {
         return checkMaxOneOdd(table);
     }
 
+    public static boolean isPermutationOfPalindromeV2(String phrase) {
+        int countOdd = 0;
+        int[] table = new int[Character.getNumericValue('z')
+                - Character.getNumericValue('a') + 1];
+
+        for (char c : phrase.toCharArray()) {
+            int x = getCharNumber(c);
+            if (x != -1) {
+                table[x]++;
+                if (table[x] % 2 == 1) {
+                    countOdd++;
+                } else {
+                    countOdd--;
+                }
+            }
+        }
+
+        return countOdd <= 1;
+    }
+
     public static boolean checkMaxOneOdd(int[] table) {
         boolean foundOdd = false;
         for (int count : table) {
