03254747ed55d3f0b771127b6e3eb248e95fb44a
Y.Yunus AKKOYUNLU <y.yunus.akkoyunlu@gmail.com>
2022-01-18 21:12:57 +0300

    Partition resolved

---
+++ preperation/crackingTheCodingInterview/chapterTwoLinkedLists/Partition.java
@@ -0,0 +1,47 @@
+package preperation.crackingTheCodingInterview.chapterTwoLinkedLists;
+
+public class Partition {
+
+    LinkedListNode partition(LinkedListNode node, int x) {
+        LinkedListNode beforeStart = null;
+        LinkedListNode beforeEnd = null;
+        LinkedListNode afterStart = null;
+        LinkedListNode afterEnd = null;
+
+        /* partition list */
+        while (node != null) {
+            LinkedListNode next = node.next;
+            node.next = null;
+            if (node.data < x) {
+                /* insert node into end of before list */
+                if (beforeStart == null) {
+                    beforeStart = node;
+                    beforeEnd = beforeStart;
+                } else {
+                    beforeEnd.next = node;
+                    beforeEnd = node;
+                }
+            } else {
+                /* insert node into end of after list */
+                if (afterStart == null) {
+                    afterStart = node;
+                    afterEnd = afterStart;
+                } else {
+                    afterEnd.next = node;
+                    afterEnd = node;
+                }
+            }
+            node = next;
+        }
+
+        if (beforeStart == null) {
+            return afterStart;
+        }
+
+        /* merge before list and after list */
+        beforeEnd.next = afterStart;
+
+        return beforeStart
+    }
+
+}
