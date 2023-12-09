760a3380d98a9f2700932eda00e8a4f7abb749b0
Yunus AKKOYUNLU <y.yunus.akkoyunlu@gmail.com>
2023-12-09 15:11:25 +0100

    code fix

--- preperation/leetcode/problemsolving/medium/SurroundedRegions.java
+++ preperation/leetcode/problemsolving/medium/SurroundedRegions.java
@@ -2,18 +2,6 @@ package preperation.leetcode.problemsolving.medium;
 
 public class SurroundedRegions {
 
-    public static void main(String[] args) {
-        char[][] board = {
-                {'X', 'X', 'X', 'X'},
-                {'X', 'O', 'O', 'X'},
-                {'X', 'X', 'X', 'X'},
-                {'X', 'O', 'O', 'X'}
-        };
-
-        solve(board);
-        System.out.println(board);
-    }
-
     public static void solve(char[][] board) {
         if (board.length == 0 || board[0].length == 0) {
             return;
